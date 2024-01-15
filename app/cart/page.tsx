5f610d81753bc54502025be7282c9521873f711e
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-01-15 19:09:08 +0530

    changes

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -7,7 +7,7 @@ import { supabase } from 'utils/supabase'
 
 export default function ShoeDetails({ params }: { params: { id: string } }) {
   // This should ideally come from your application's state (e.g., Redux, Context API)
-  if (window !== undefined) {
+  if (window === undefined) {
     return <></>
   }
   const cartItems = JSON.parse(localStorage.getItem('cart_array') ?? '[]')
